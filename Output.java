d85c99ed36d25b8abf325ad69609102d3e930ace
baoyixin <997901744@qq.com>
2019-11-02 18:29:31 +0800

    多线程通信

---
+++ Output.java
@@ -0,0 +1,12 @@
+
+public class Output implements Runnable {
+      private Storage st;
+      Output(Storage st){
+    	  this.st=st;
+      }
+      public void run() {
+    	  while(true) {
+    		  st.get();
+    	  }
+      }
+}
