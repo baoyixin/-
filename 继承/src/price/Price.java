a8b7074e3657a1b3fb5f800fd7214234b1a6daa9
baoyixin <997901744@qq.com>
2019-11-30 22:48:05 +0800

    重构之继承

---
+++ 继承/src/price/Price.java
@@ -0,0 +1,15 @@
+package price;
+
+import movie.Movie;
+
+public abstract class Price {
+
+	public abstract int getPriceCode();
+
+	public abstract double getCharge(int daysRented);
+
+	public int getFrequentRenterPoints(int daysRented) {
+		
+			return 1;
+	}
+}
